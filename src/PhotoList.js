a9f2b8f2c1398bd23c8b254940da7c86503cfe89
Mukhammadali <ali_6017@mail.ru>
2022-06-10 22:55:38 +0500

    Photos.js-styles

---
+++ src/PhotoList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Image from "./Image";
+
+const PhotoList = ({ photos }) => {
+  return (
+    <div className="tc">
+      {photos.map((photo, idx) => {
+        return (
+          <Image
+            key={idx}
+            id={photos[idx].id}
+            url={photos[idx].url}
+            name={photos[idx].name}
+            email={photos[idx].email}
+            info={photos[idx].info}
+          />
+        );
+      })}
+    </div>
+  );
+};
+export default PhotoList;
